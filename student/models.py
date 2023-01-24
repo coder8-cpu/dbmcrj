431cc3440a781f90759b32cfa24439f6c388cc48
coder8-cpu <debashisharkar90072@gmail.com>
2023-01-23 18:30:54 -0600

    change in models

--- student/models.py
+++ student/models.py
@@ -4,74 +4,70 @@ from uuid import uuid4
 
 class StudentSignup(models.Model):
     
-    Rollno = models.CharField(max_length=1000)
-    name = models.CharField(max_length=1000)
-    password = models.CharField(max_length=1000000)
+    Rollno = models.CharField(max_length=1000,null=True,blank=True)
+    name = models.CharField(max_length=1000,null=True,blank=True)
+    password = models.CharField(max_length=1000000,null=True,blank=True)
 
 class StudentData(models.Model):
     
-    year = models.CharField(max_length=1000)
-    rollno = models.CharField(max_length=1000)
-    name = models.CharField(max_length=1000)
-    semester = models.CharField(max_length=1000)
-    branch = models.CharField(max_length=1000)
-    semester_marksheet = models.FileField()
-    semester_registration = models.FileField()
-    semester_routine = models.FileField()
-    semester_admit = models.FileField()
-    course_type = models.CharField(max_length=1000)
+    year = models.CharField(max_length=1000,null=True,blank=True)
+    rollno = models.CharField(max_length=1000,null=True,blank=True)
+    name = models.CharField(max_length=1000,null=True,blank=True)
+    semester = models.CharField(max_length=1000,null=True,blank=True)
+    branch = models.CharField(max_length=1000,null=True,blank=True)
+    course_type = models.CharField(max_length=1000,null=True,blank=True)
 
     
 class Marksheets(models.Model):
-    year = models.CharField(max_length=1000)
-    semester      = models.CharField(max_length=1000)
-    branch        = models.CharField(max_length=1000)
-    marksheet_pdf = models.FileField(upload_to="media/files/marksheets")
-    roll        = models.CharField(max_length=1000)
-    course_type = models.CharField(max_length=1000)
+    year = models.CharField(max_length=1000,null=True,blank=True)
+    semester      = models.CharField(max_length=1000,null=True,blank=True)
+    branch        = models.CharField(max_length=1000,null=True,blank=True)
+    marksheet_pdf = models.FileField(upload_to="media/files/marksheets",null=True,blank=True)
+    roll        = models.CharField(max_length=1000,null=True,blank=True)
+    course_type = models.CharField(max_length=1000,null=True,blank=True)
 
     
 
 
 class routine(models.Model):
-    year          = models.CharField(max_length=1000)
-    semester      = models.CharField(max_length=1000)
-    branch        = models.CharField(max_length=1000)
-    day           = models.CharField(max_length=1000)
-    time          = models.CharField(max_length=1000)
-    subject       = models.CharField(max_length=1000)
-    teacher       = models.CharField(max_length=1000)
+    year          = models.CharField(max_length=1000,null=True,blank=True)
+    semester      = models.CharField(max_length=1000,null=True,blank=True)
+    branch        = models.CharField(max_length=1000,null=True,blank=True)
+    day           = models.CharField(max_length=1000,null=True,blank=True)
+    time          = models.CharField(max_length=1000,null=True,blank=True)
+    subject       = models.CharField(max_length=1000,null=True,blank=True)
+    teacher       = models.CharField(max_length=1000,null=True,blank=True)
     course_type = models.CharField(max_length=1000)
 
 
 class Registration(models.Model):
-    year             = models.CharField(max_length=1000)
-    semester         = models.CharField(max_length=1000)
-    branch           = models.CharField(max_length=1000)
-    registration_pdf = models.FileField(upload_to="media/files/registration")
-    roll             = models.CharField(max_length=1000)
-    course_type = models.CharField(max_length=1000)
+    year             = models.CharField(max_length=1000,null=True,blank=True)
+    semester         = models.CharField(max_length=1000,null=True,blank=True)
+    branch           = models.CharField(max_length=1000,null=True,blank=True)
+    registration_pdf = models.FileField(upload_to="media/files/registration",null=True,blank=True)
+    roll             = models.CharField(max_length=1000,null=True,blank=True)
+    course_type = models.CharField(max_length=1000,null=True,blank=True)
 
 
 class AdmitCard(models.Model):
-    year = models.CharField(max_length=1000)
-    semester      = models.CharField(max_length=1000)
-    branch        = models.CharField(max_length=1000)
-    admit_pdf     = models.FileField(upload_to="media/files/admitcard")
-    roll          = models.CharField(max_length=1000)
-    course_type = models.CharField(max_length=1000)
+    year = models.CharField(max_length=1000,null=True,blank=True)
+    semester      = models.CharField(max_length=1000,null=True,blank=True)
+    branch        = models.CharField(max_length=1000,null=True,blank=True)
+    admit_pdf     = models.FileField(upload_to="media/files/admitcard",null=True,blank=True)
+    roll          = models.CharField(max_length=1000,null=True,blank=True)
+    course_type = models.CharField(max_length=1000,null=True,blank=True)
 
 
 
 class ExamRoutine(models.Model):
-    year = models.CharField(max_length=1000)
-    semester      = models.CharField(max_length=1000)
-    branch        = models.CharField(max_length=1000)
-    exam_pdf     = models.FileField(upload_to="media/files/admitcard")
-    course_type = models.CharField(max_length=1000)
+    year = models.CharField(max_length=1000,null=True,blank=True)
+    semester      = models.CharField(max_length=1000,null=True,blank=True)
+    branch        = models.CharField(max_length=1000,null=True,blank=True)
+    exam_pdf     = models.FileField(upload_to="media/files/admitcard",null=True,blank=True)
+    course_type = models.CharField(max_length=1000,null=True,blank=True)
 
 class Syllabus(models.Model):
-    year = models.CharField(max_length=1000)
+    year = models.CharField(max_length=1000,null=True,blank=True)
     semester      = models.CharField(max_length=1000)
     branch        = models.CharField(max_length=1000)
     syllabus_pdf     = models.FileField(upload_to="media/files/admitcard")
@@ -80,19 +76,19 @@ class Syllabus(models.Model):
 
 class IdentityCard(models.Model):
     id = models.BigAutoField(primary_key=True,default=uuid4,editable=False)
-    year = models.CharField(max_length=1000)
-    rollno = models.CharField(max_length=1000)
-    name = models.CharField(max_length=1000)
-    semester = models.CharField(max_length=1000)
-    branch = models.CharField(max_length=1000)
-    course_type = models.CharField(max_length=1000)
-    address  = models.CharField(max_length=1000)
-    blood_group = models.CharField(max_length=1000)
-    registrationNo = models.CharField(max_length=1000)
+    year = models.CharField(max_length=1000,null=True,blank=True)
+    rollno = models.CharField(max_length=1000,null=True,blank=True)
+    name = models.CharField(max_length=1000,null=True,blank=True)
+    semester = models.CharField(max_length=1000,null=True,blank=True)
+    branch = models.CharField(max_length=1000,null=True,blank=True)
+    course_type = models.CharField(max_length=1000,null=True,blank=True)
+    address  = models.CharField(max_length=1000,null=True,blank=True)
+    blood_group = models.CharField(max_length=1000,null=True,blank=True)
+    registrationNo = models.CharField(max_length=1000,null=True,blank=True)
      
 class RegisterRollNo(models.Model):
    
-    name = models.CharField(max_length=1000)
+    name = models.CharField(max_length=1000,null=True,blank=True)
 
     def __str__(self) -> str:
         return str(self.name)
